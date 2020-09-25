a5224be7aa2e578a09e2c11fbc00bce662385ae5
Александр Голубев <aleksandrgolubev@MacBook-Pro-Aleksandr.local>
2020-09-25 17:26:51 +0300

    config updated

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 MARKUP_PATH = './markup_test.xls' #path to the markup
 FPATH = '.' #path to the video trailers
 TRAINING_PATH = './train' #path to final train folder
+TEST_TRAILER = './85.Дикая.mp4'
+PATH_MODEL_CHECKPOINT = './'
 
