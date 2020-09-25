7da66fcff87de0da0ae6737409ebaf05b2561e78
Александр Голубев <aleksandrgolubev@MacBook-Pro-Aleksandr.local>
2020-09-25 17:27:57 +0300

    Arguments to train function added

--- train.py
+++ train.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import numpy as np
 import pandas as pd
 import random
 import os
 import torch
-from torchvision import datasets, transforms
-from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets
+#from torch.utils.tensorboard import SummaryWriter
 from model import LRCN
 from loading_data import load_data
+from config import TRAINING_PATH, PATH_MODEL_CHECKPOINT
 
 def init_model(device='cpu'):
     model = LRCN()
@@ -22,70 +17,72 @@ def init_model(device='cpu'):
     model.to(device) 
     return model, (loss, mae), optimizer
 
-def create_test(dir_names, device='cpu', verbose=True, batch_size=100):
-    X_test, y_test, dir_names = load_data(dir_names, verbose=True, batch_size=100)
-    X_test = X_test.to(device)
-    y_test = y_test.to(device)
+def create_test(dir_names, device='cpu', verbose=False, batch_size=100):
+    X_test, y_test, dir_names = load_data(dir_names, verbose=True, batch_size=batch_size)
+    X_test = X_test.to(device).float()
+    y_test = y_test.to(device).float()
     
-    return X_test, y_test
+    return X_test, y_test, dir_names
 
-def train():
-
-    tb = SummaryWriter()
+def train(dir_names, X_test, y_test, n_epoch=5, batch_size=10, device='cpu', use_checkpoint=False, use_tensorb=False, verbose=False):
+    
+#     if use_tensorb:
+#         tb = SummaryWriter()
+        
     model, Loss, optimizer = init_model()
     loss, mae = Loss[0], Loss[1]
-    dir_names = os.listdir(f'{TRAINING_PATH}')
+    
+    dir_names = list(filter(lambda x: os.path.isdir(f"{TRAINING_PATH}/{x}"), dir_names)) #Filtering waste files
     random.shuffle(dir_names)
-    
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    
-    X_test, y_test = create_test(dir_names, device, verbose, batch_size)
 
-    batch_size = 10
-    test_rmse_history = []
+    train_mse_history = []
+    test_mse_history = []
     test_mae_history = []
-    PATH = "/content/checkpoint.tar"
-    checkpoint = True
-    tensorboard = True
 
     learning_dir_names = dir_names.copy()
     #Training model
-    for epoch in range(7):
+    for epoch in range(n_epoch):
         dir_names = learning_dir_names.copy()
+        train_loss = 0
         for i in range(0, len(learning_dir_names), batch_size):
             optimizer.zero_grad()
-            X_batch, y_batch, dir_names = load_data(dir_names, verbose=True, batch_size=batch_size)  
+            X_batch, y_batch, dir_names = load_data(dir_names, verbose, batch_size=batch_size)  
 
-            X_batch = X_batch.to(device)
-            y_batch = y_batch.to(device)
+            X_batch = X_batch.to(device).float()
+            y_batch = y_batch.to(device).float()
 
             preds = model.forward(X_batch).view(y_batch.size()[0])
-            loss_value = torch.sqrt(loss(preds, y_batch)) #Converting to RMSE
+            loss_value = loss(preds, y_batch)
             loss_value.backward()
-
+            
+            train_loss += loss_value.data.cpu()
             optimizer.step()
+            
+        train_mse_history.append(train_loss)
 
         with torch.no_grad():
             test_preds = model.forward(X_test).view(y_test.size()[0])
-            test_rmse_history.append(loss(test_preds, y_test).data.cpu())
+            test_mse_history.append(loss(test_preds, y_test).data.cpu())
             test_mae_history.append(mae(test_preds, y_test).data.cpu())
 
         #Saving checkpoint
-        if checkpoint:
+        if use_checkpoint:
             torch.save({
                     'epoch': epoch,
                     'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict(),
-                    'rmse': test_rmse_history[-1],
+                    'mse': test_mse_history[-1],
                     'mae': test_mae_history[-1],
-                    }, PATH)
+                    }, PATH_MODEL_CHECKPOINT)
 
-        if tensorboard:
-            tb.add_scalar('RMSE', test_rmse_history[-1], epoch)
-            tb.add_scalar('MAE', test_mae_history[-1], epoch)
-        print(f"{epoch+1}: RMSE = {test_rmse_history[-1]}")
-        print(f"{epoch+1}: MAE = {test_mae_history[-1]}")
+#         if use_tensorb:
+#             tb.add_scalar('MSE', test_rmse_history[-1], epoch)
+#             tb.add_scalar('MAE', test_mae_history[-1], epoch)
 
-    tb.close()
-    return test_history
+        print(f"{epoch+1}: MSE = {test_mse_history[-1]}, MAE = {test_mae_history[-1]}")
+       
+     #if use_tensorb:
+     #    tb.close()
+        
+    return [train_mse_history, test_mse_history]
 
