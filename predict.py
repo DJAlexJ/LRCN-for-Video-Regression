e2671102078c9ddb5a306476959f77e41ff1c43f
Александр Голубев <aleksandrgolubev@MacBook-Pro-Aleksandr.local>
2020-10-13 21:44:11 +0300

    train_predict scripts

--- predict.py
+++ predict.py
@@ -1,17 +1,18 @@
 import argparse
 import preprocessing as prep
 import torch
+import os
 from loading_data import load_data
 from model import CNN, LRCN
-from config import PREDICTION_PATH, TEST_TRAILER_NAME, PATH_MODEL_CHECKPOINT
+from config import PREDICTION_PATH
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg("-i", "--input_path", type=Path, help="Path with movie trailers", required=True)
-    arg("-out", "--output_path", type=Path, help="Path to output file with movie scores", required=True)
-    arg("-w", "--weights", type=Path, help="Path to weights", required=True)
+    arg("-i", "--input_path", type=str, help="Path with movie trailers", required=True)
+    arg("-out", "--output_path", type=str, help="output file (with path) with movie scores", required=True)
+    arg("-w", "--weights", type=str, help="Path to weights", required=True)
     arg("-v", "--verbose", default=False, help="Verbosity")
     arg("-d", "--device", default='cpu', type=str, help="Device for computations")
     arg("-vis", "--visualize", action="store_true", help="Visualize results")
@@ -21,25 +22,16 @@ def get_args():
 def predict():
     args = get_args()
     
-    hparams = {
-            "input_path": args.input_path,
-            "output_path": args.output_path,
-            "weights": args.weights,
-            "verbose": args.verbose,
-            "device": args.device,
-            "visualize": args.visualize,
-        }
-    
     model = LRCN()
-    model.load_state_dict(torch.load(hparams['weights'], map_location=torch.device(hparams['device'])))
+    model.load_state_dict(torch.load(args.weights, map_location=torch.device(args.device)))
     model.eval()
     
-    prep.movies_preporcess(os.listdir(hparams['input_path']), train=False, n_subclips=1, verbose=hparams['verbose'])
+    prep.movies_preprocess(os.listdir(args.input_path), train=False, n_subclips=1, verbose=args.verbose)
     dir_names = os.listdir(PREDICTION_PATH)
-    dir_names = list(filter(lambda x: os.path.isdir(f"{path}/{x}"), dir_names))
+    dir_names = list(filter(lambda x: os.path.isdir(f"{PREDICTION_PATH}/{x}"), dir_names))
     
-    predictions = model.predict(dir_names, verbose=hparams['verbose'], saving_results=True, 
-                                output_path=hparams['output_path'])
+    predictions = model.predict(dir_names, verbose=args.verbose, saving_results=True,
+                                output_path=args.output_path)
     
     return predictions
 
--- train.py
+++ train.py
@@ -5,122 +5,52 @@ import os
 import torch
 from torchvision import datasets
 import argparse
+import preprocessing as prep
+import loading_data as ld
 #from torch.utils.tensorboard import SummaryWriter
 from model import LRCN
 from loading_data import load_data
-from config import TRAINING_PATH, PATH_MODEL_CHECKPOINT
+from config import TRAINING_PATH
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg("-i", "--input_path", type=Path, help="Path with training trailers", required=True)
-    arg("-out", "--output_path", type=Path, help="Path to output file with movie scores", required=True)
+    arg("-i", "--input_path", type=str, help="Path with training trailers", required=True)
     arg("-l", "--loss", type=str, default='mse', help="Loss function")
-    arg("-a", "--activation", type=str, default='relu, help="Activation function")
+    arg("-a", "--activation", type=str, default='relu', help="Activation function")
     arg("-ne", "--n_epoch", type=int, default=7, help="Number of epochs")
     arg("-bs", "--batch_size", type=int, default=10, help="Batch size")
-    arg("-lr", "--learning_rate", type=int, default=3e-4, help="Learning rate")
-    arg("-ns", "--n_subclips", type=int, default=3, "Number of sublcips extracted from each video")
+    arg("-tbs", "--test_batch_size", type=int, default=1, help="Batch size for test data")
+    arg("-lr", "--learning_rate", type=float, default=3e-4, help="Learning rate")
+    arg("-ns", "--n_subclips", type=int, default=3, help="Number of sublcips extracted from each video")
+    arg("-sw", "--save_weights", default=True, help="Whether to save model weights")
     arg("-prep", "--preprocessing", default=True, help="Whether to perform preprocessing")
     arg("-v", "--verbose", default=False, help="Verbosity")
     arg("-d", "--device", default='cpu', type=str, help="Device for computations")
-    arg("-vis", "--visualize", action="store_true", help="Visualize results")
-  
+    arg("-vis", "--visualize", default=False, action="store_true", help="Visualize results")
+    return parser.parse_args()
 
 def train():
     args = get_args()
     
-    hparams = {
-            "input_path": args.input_path,
-            "output_path": args.output_path,
-            "loss": args.loss,
-            "activation": args.activation,
-            "n_epoch": args.n_epoch,
-            "batch_size": args.batch_size,
-            "n_subclips": args.n_subclips,
-            "preprocessing": args.preprocessing,
-            "verbose": args.verbose,
-            "device": args.device,
-            "visualize": args.visualize,
-        }
-    
-    if hparams['preprocessing'] == True:
-        prep.movies_preprocess(os.listdir(hparams['input_path'], train=True, n_subclips=hparams['n_subclips'], verbose=hparams['verbose'])
+    if args.preprocessing == True:
+        prep.movies_preprocess(os.listdir(args.input_path), train=True, n_subclips=args.n_subclips, verbose=args.verbose)
                                
-    model = LRCN(activation=hparams['activation'])
-    model.to(hparams['device'])
+    model = LRCN(activation=args.activation)
+    model.to(args.device)
                                
     dir_names = os.listdir(TRAINING_PATH)
-    dir_names = list(filter(lambda x: os.path.isdir(f"{TRAINING_PATH}/{x}"), dir_names)) #Filtering waste files
+    print(dir_names)
 
-    X_test, y_test, dir_names = ld.load_data(dir_names, train=True, verbose=hparams['verbose'], batch_size=1)
-    model.train()
-                               
-                               
-
-def train(dir_names, X_test, y_test, n_epoch=5, batch_size=10, device='cpu', use_checkpoint=False, use_tensorb=False, verbose=False):
+    X_test, y_test, dir_names = ld.load_data(dir_names, train=True, verbose=args.verbose, batch_size=args.test_batch_size)
     
-#     if use_tensorb:
-#         tb = SummaryWriter()
-        
-    model, Loss, optimizer = init_model()
-    loss, mae = Loss[0], Loss[1]
-    
-    dir_names = list(filter(lambda x: os.path.isdir(f"{TRAINING_PATH}/{x}"), dir_names)) #Filtering waste files
-    random.shuffle(dir_names)
-
-    train_mse_history = []
-    test_mse_history = []
-    test_mae_history = []
-
-    learning_dir_names = dir_names.copy()
-    #Training model
-    for epoch in range(n_epoch):
-        dir_names = learning_dir_names.copy()
-        train_loss = 0
-        for i in range(0, len(learning_dir_names), batch_size):
-            optimizer.zero_grad()
-            X_batch, y_batch, dir_names = load_data(dir_names, verbose, batch_size=batch_size)  
-
-            X_batch = X_batch.to(device).float()
-            y_batch = y_batch.to(device).float()
-
-            preds = model.forward(X_batch).view(y_batch.size()[0])
-            loss_value = loss(preds, y_batch)
-            loss_value.backward()
-            
-            train_loss += loss_value.data.cpu()
-            optimizer.step()
-            
-        train_mse_history.append(train_loss)
-
-        with torch.no_grad():
-            test_preds = model.forward(X_test).view(y_test.size()[0])
-            test_mse_history.append(loss(test_preds, y_test).data.cpu())
-            test_mae_history.append(mae(test_preds, y_test).data.cpu())
-
-        #Saving checkpoint
-        if use_checkpoint:
-            torch.save({
-                    'epoch': epoch,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'mse': test_mse_history[-1],
-                    'mae': test_mae_history[-1],
-                    }, PATH_MODEL_CHECKPOINT)
-
-#         if use_tensorb:
-#             tb.add_scalar('MSE', test_rmse_history[-1], epoch)
-#             tb.add_scalar('MAE', test_mae_history[-1], epoch)
-
-        print(f"{epoch+1}: MSE = {test_mse_history[-1]}, MAE = {test_mae_history[-1]}")
-       
-     #if use_tensorb:
-     #    tb.close()
-        
-    return [train_mse_history, test_mse_history]
+    model.fit(dir_names, X_test, y_test, lr=args.learning_rate,                     loss_name=args.loss, n_epoch=args.n_epoch,
+                batch_size=args.batch_size, device=args.device,
+                verbose=args.verbose, saving_results=args.save_weights)
                                
+                               
+
 
 if __name__ == "__main__":
     train()
